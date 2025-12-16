4bb3cfbf7ca1fbec6c2207d0f65b7d7630f5b7ed
CesarBE0 <cesarbelest@gmail.com>
2025-12-16 16:27:54 +0100

    comit

--- app/ejemplos/biblioteca_v2/README.txt
+++
@@ -1,5 +0,0 @@
-
-Versión preparada para backend (genérica)
-- Formularios en contacto.html y opiniones.html hacen POST a /api/contact y /api/reviews respectivamente.
-- En catalogo.html el formulario de adición de libro (id=add-book-form) está preparado para enviar campos: title, author, type, pages.
-- Implementa en tu backend rutas POST para manejar estos endpoints y persistir en la DB.
--- app/login.php
+++ app/login.php
@@ -93,7 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lectio - Acceso</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
@@ -161,6 +161,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 </div>
 
-<script src="js/login.js"></script>
+<script src="assets/js/login.js"></script>
 </body>
 </html>
